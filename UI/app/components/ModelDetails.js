168b4dd74d7d543293b4607cb96ee64da8a290ef
sayakbiswas <sayak90@ufl.edu>
2017-01-31 01:14:19 -0500

    Completed Model Details screen mockup

--- UI/app/components/ModelDetails.js
+++ UI/app/components/ModelDetails.js
@@ -19,7 +19,10 @@ function ModelDetails(props) {
 						                <div className="eight wide column left aligned modelname">
                                             <img src="app/img/model_icon.png" className="modelicon"/>3-Layer CNN
                                             <div className="modeldescription">
-                                                Headline of 3-Layer CNN
+                                                A CNN for image tagging
+                                                <span className="ui label">
+                                                    Vision
+                                                </span>
                                             </div>
                                             <div className="modeldownload">
                                                 <DownloadButtonContainer
@@ -27,18 +30,21 @@ function ModelDetails(props) {
 											        shouldDisplayButton={true} />
                                             </div>
                                         </div>
-                                        <div className="eight wide column right aligned">
-                                            Rating:
-                                            <span className="ui star rating" data-rating="4" data-max-rating="5"></span>
+                                        <div className="eight wide column right aligned modelinfo">
+                                            <div>
+                                                Rating:
+                                                <span className="ui star rating" data-rating="4" data-max-rating="5"></span>
+                                            </div>
+                                            <div className="devdetails">
+                                                Developed By:
+                                                Alex<img className="ui avatar image" src="app/img/usr_img_1.png" />
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="column modelexplanation">
                                             <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id leo pulvinar, faucibus nibh et, fermentum ante. Donec ac convallis lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut libero elementum, efficitur risus ac, eleifend nisi. Mauris rutrum ultrices magna, id finibus libero commodo ac. Nullam massa ex, ullamcorper a maximus condimentum, laoreet vel tortor. Vestibulum commodo odio nec massa sagittis volutpat. Vestibulum vel consectetur eros, non feugiat neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In metus quam, laoreet sit amet pretium quis, auctor ac velit. Phasellus pellentesque non ligula vehicula tempor. Nunc varius turpis sit amet enim cursus maximus. Vestibulum justo libero, posuere sed est non, sagittis pretium tellus.
-                                            </p>
-                                            <p>
-                                                Duis porta odio sed urna aliquet, sit amet lobortis neque placerat. Mauris nisi ligula, tincidunt et scelerisque in, luctus sed lectus. Pellentesque luctus mauris suscipit lacus sodales, quis varius sem suscipit. Sed ac faucibus augue. Phasellus rutrum condimentum tortor, vitae viverra diam commodo id. Ut euismod mi orci. Fusce sed sodales enim, eu pretium eros. Sed ante nunc, tincidunt ornare nisi at, scelerisque hendrerit augue. Donec justo libero, posuere sed leo sed, pellentesque consequat elit. In bibendum felis quis felis facilisis scelerisque. Aenean mollis lorem eu dui pharetra, ac ultricies purus laoreet. Pellentesque iaculis sem id nisl consectetur eleifend.
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id leo pulvinar, faucibus nibh et, fermentum ante. Donec ac convallis lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut libero elementum, efficitur risus ac, eleifend nisi. Mauris rutrum ultrices magna, id finibus libero commodo ac. Nullam massa ex, ullamcorper a maximus condimentum, laoreet vel tortor. Vestibulum commodo odio nec massa sagittis volutpat. Vestibulum vel consectetur eros, non feugiat neque. className aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In metus quam, laoreet sit amet pretium quis, auctor ac velit. Phasellus pellentesque non ligula vehicula tempor. Nunc varius turpis sit amet enim cursus maximus. Vestibulum justo libero, posuere sed est non, sagittis pretium tellus.
                                             </p>
                                         </div>
                                     </div>
@@ -50,9 +56,10 @@ function ModelDetails(props) {
                                             </div>
                                             <div className="paperDownload">
                                                 Associated Paper:
-                                                <DownloadButtonContainer
-											        modelDownloadLink=""
-											        shouldDisplayButton={true} />
+                                                <button
+                                                    className="ui blue button" onClick={props.onClickDownload}>
+                                                    Download Paper
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
@@ -65,7 +72,64 @@ function ModelDetails(props) {
                                 <div className="ui grid">
                                     <div className="row">
                                         <div className="column">
-                                            <div className="header">Comments</div>
+                                            <div className="ui comments">
+                                                <h3 className="ui dividing header">Reviews</h3>
+                                                <div className="comment">
+                                                    <a className="avatar">
+                                                        <img src="app/img/usr_img_1.png" />
+                                                    </a>
+                                                    <div className="content">
+                                                        <a className="author">Matt</a>
+                                                        <span className="ui star rating" data-rating="3" data-max-rating="5"></span>
+                                                        <div className="metadata">
+                                                            <span className="date">Today at 5:42PM</span>
+                                                        </div>
+                                                        <div className="text">
+                                                            This model saved a lot of time in my research. Also, very well documented!
+                                                        </div>
+                                                        <div className="actions">
+                                                            <a className="report">Report</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="comment">
+                                                    <a className="avatar">
+                                                        <img src="app/img/usr_img_2.png" />
+                                                    </a>
+                                                    <div className="content">
+                                                        <a className="author">Elliot Fu</a>
+                                                        <span className="ui star rating" data-rating="5" data-max-rating="5"></span>
+                                                        <div className="metadata">
+                                                            <span className="date">Yesterday at 12:30AM</span>
+                                                        </div>
+                                                        <div className="text">
+                                                            <p>The accuracy of this model is great. I trained it for 3 hrs and got an accuracy of around 98%.
+                                                    Will play around with it by changing the hyper-parameters.</p>
+                                                        </div>
+                                                        <div className="actions">
+                                                            <a className="report">Report</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <form className="ui reply form">
+                                                    <div className="field">
+                                                        <textarea></textarea>
+                                                    </div>
+                                                    <div className="ui grid">
+                                                        <div className="row">
+                                                            <div className="eight wide column left aligned ratings">
+                                                                <b>Rating:</b>
+                                                                <span className="ui star rating" data-rating="3" data-max-rating="5"></span>
+                                                            </div>
+                                                            <div className="eight wide column right aligned">
+                                                                <div className="ui blue labeled submit icon button">
+                                                                    <i className="icon edit"></i> Add Reviews
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
